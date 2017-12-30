b53156a09e68656e1b4894fd994358b4c6372a51
Daniel <dds2130@rit.edu>
2017-12-29 22:06:51 -0500

    added locations

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var Jimp = require("jimp");
 var fs = require('fs');
 
-var base = "C:/Users/Daniel/Google Drive/Chaotic_Backup/Mugic/"
-var tribe = "UnderWorld/";
+var base = "C:/Users/Daniel/Google Drive/Chaotic_Backup/Locations/"
+var tribe = "";
 var directory = base + "Card/" + tribe;
 
 var files = fs.readdirSync(directory);
 
+
+// All Horizontal Cards (not loctions)
+/*
 files.forEach(function(item) {
 	Jimp.read(directory+item).then(function (image) {
 		image.crop(12, 37, 239-12, 206-37)
@@ -20,4 +23,20 @@ files.forEach(function(item) {
 	    console.log(item, err);
 	});;
 });
+*/
 
+// Locations
+files.forEach(function(item) {
+	Jimp.read(directory+item).then(function (image) {
+		image.crop(36, 100, 162-36, 250-100)
+		image.rotate(90);
+		image.cover(100, 96);
+		if (!fs.existsSync(base+"Thumbnail/"+tribe+item)) {
+			// console.log("write")
+			image.write(base+"Thumbnail/"+tribe+item);
+		}
+	}).catch(function (err) {
+	    // handle an exception
+	    console.log(item, err);
+	});;
+});
